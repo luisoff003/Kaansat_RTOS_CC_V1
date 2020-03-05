725538beb31fb3733d59cbc36a3e44d9b5ab72a9
Luison <4769@soyunaq.mx>
2020-03-04 19:56:03 -0600

    XML removed
    Remove XML

--- source/main.c
+++ source/main.c
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include "param.h"
@@ -8,3 +9,15 @@ int main()
     printf("%0.4f\n",P.Jx);
     return 0;
 }
+=======
+#include <stdio.h>
+#include <stdlib.h>
+#include "param.h"
+
+int main()
+{
+    InitParam( &P);
+    printf("%0.4f\n",P.Jx);
+    return 0;
+}
+>>>>>>> parent of 20aa473... no main
